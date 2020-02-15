355dd1a2078a0ae1722d2a0931901625080471cd
Alex J <abel.oalex@gmail.com>
2020-02-15 18:35:27 -0300

    Updates

---
+++ precache-manifest.5ea335ac241355a6be229a777de57a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729c0d647c16b0300e7e61ee8c8ec774",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0151d592de5294638167",
+    "url": "/monsters-rolodex/static/css/main.2aa22644.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0151d592de5294638167",
+    "url": "/monsters-rolodex/static/js/main.035718d4.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
