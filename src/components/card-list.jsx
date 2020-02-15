695233f7bd5d1cbe46101b1bdc886472c6cb71e0
Alex J <abel.oalex@gmail.com>
2020-02-15 14:23:10 -0300

    added card component and images

--- src/components/card-list.jsx
+++ src/components/card-list.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { Card } from "./card";
+
 import './card-list.css'
 
 export const CardList = (props) => {
-    return <div className={'card-list'}>{ props.monsters.map((element) => <h1 key={element.id}>{element.name}</h1>) }</div>
+    return  <div className={'card-list'}>
+                { props.monsters.map((monster) =>
+                    <Card key={monster.id} monster={monster}></Card>) }
+            </div>
 };
